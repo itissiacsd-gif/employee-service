1f2880da434c8a20316d35050890a61a1bac0984
itissiacsd-gif <itissiacds@gmail.com>
2026-07-26 18:19:31 +0530

    Use Lombok for EmployeeRequest DTO

--- src/main/java/com/devsecops/employeeservice/dto/EmployeeRequest.java
+++ src/main/java/com/devsecops/employeeservice/dto/EmployeeRequest.java
@@ -2,7 +2,9 @@ package com.devsecops.employeeservice.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
 
+@Data
 public class EmployeeRequest {
 
     @NotBlank
@@ -15,36 +17,4 @@ public class EmployeeRequest {
     private String department;
 
     private Double salary;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public Double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(Double salary) {
-        this.salary = salary;
-    }
 }
